a9a3d3a8793fd84b16aa45e4fd875dc3035765ab
Calvin Tan <hello@calvin-tan.com>
2019-09-01 23:07:50 +0800

    Add extra dummy data, set a upper/lower limit for navigating left and right

--- js/app.js
+++ js/app.js
@@ -14,6 +14,30 @@ const data = [
   {
     title: "four",
     content: "fourth content"
+  },
+  {
+    title: "5",
+    content: "5 content"
+  },
+  {
+    title: "6",
+    content: "6 content"
+  },
+  {
+    title: "7",
+    content: "7 content"
+  },
+  {
+    title: "8",
+    content: "8 content"
+  },
+  {
+    title: "9",
+    content: "9 content"
+  },
+  {
+    title: "",
+    content: ""
   }
 ]
 
@@ -22,19 +46,28 @@ const leftBtn = document.querySelector('#left-btn');
 const rightBtn = document.querySelector('#right-btn');
 const slideIndicator = document.querySelector('.slide-indicator');
 
+let current = 0;
 let move = 0;
 let moveAmount = 1100;
 
 leftBtn.addEventListener('click', function() {
-  console.log('left');
-  move += moveAmount;
-  bg.style.transform = `translate(${move}px)`;
+  current--;
+  if (current < 0) {
+    current = 0;
+  } else {
+    move += moveAmount;
+    bg.style.transform = `translate(${move}px)`;
+  }
 });
 
 rightBtn.addEventListener('click', function() {
-  console.log('right');
-  move -= moveAmount;
-  bg.style.transform = `translate(${move}px)`;
+  current++;
+  if (current >= data.length) {
+    current = data.length - 1;
+  } else {
+    move -= moveAmount;
+    bg.style.transform = `translate(${move}px)`;
+  }
 });
 
 data.forEach((item, index) => {
@@ -45,6 +78,7 @@ const indicators = document.querySelectorAll('.slide-indicator span');
 
 indicators.forEach((item, index) => {
   item.addEventListener('click', function(){
+    current = index;
     move = moveAmount * index * -1;
     bg.style.transform = `translate(${move}px)`;
   })
