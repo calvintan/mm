0f808db2b514ed12a6927670e98a55f346d0579d
Calvin Tan <hello@calvin-tan.com>
2019-09-07 19:30:04 +0800

    Rename html classes, refactor css, update functions to es5

--- js/app.js
+++ js/app.js
@@ -1,52 +1,8 @@
-const data = [
-  {
-    title: "one",
-    content: "first content"
-  },
-  {
-    title: "two",
-    content: "second content"
-  },
-  {
-    title: "three",
-    content: "third content"
-  },
-  {
-    title: "four",
-    content: "fourth content"
-  },
-  {
-    title: "5",
-    content: "5 content"
-  },
-  {
-    title: "6",
-    content: "6 content"
-  },
-  {
-    title: "7",
-    content: "7 content"
-  },
-  {
-    title: "8",
-    content: "8 content"
-  },
-  {
-    title: "9",
-    content: "9 content"
-  },
-  {
-    title: "",
-    content: ""
-  }
-]
-
-const bg = document.querySelector('.bg-layer img');
+const bg = document.querySelector('.bg img');
 const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
 const slideIndicator = document.querySelector('.slide-indicator');
-const textLayer = document.querySelector('.text-layer');
-const texts = document.querySelectorAll('.text-layer div');
+const texts = document.querySelectorAll('.texts div');
 
 let current = 0;
 let move = 0;
@@ -83,14 +39,14 @@ nextBtn.addEventListener('click', function() {
   }
 });
 
-data.forEach((item, index) => {
+texts.forEach(function(item, index) {
   slideIndicator.insertAdjacentHTML('beforeend', `<span class="indicator">${index}</span>`);
 });
 
-const indicators = document.querySelectorAll('.slide-indicator span');
+const indicators = document.querySelectorAll('.indicator');
 
-indicators.forEach((item, index) => {
-  item.addEventListener('click', function(){
+indicators.forEach(function(item, index) {
+  item.addEventListener('click', function() {
     currentText = texts[current];
     current = index;
     move = moveAmount * index * -1;
@@ -99,6 +55,6 @@ indicators.forEach((item, index) => {
     bg.addEventListener('transitionend', () => {
       texts[current].classList.add('active');
     });
-  })
+  });
 });
 
